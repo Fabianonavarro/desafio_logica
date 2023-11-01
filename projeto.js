0beeceed4dfeb2169f60618713ff1bd281c74f60
Fabianonavarro <126501633+Fabianonavarro@users.noreply.github.com>
2023-11-01 12:16:50 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,24 @@
+console 
+//let XP = 1000
+let XP = 0
+let xp = prompt('Digite o seu xp:')
+if(XP <= 1000){
+console.log("Seu nivel e : Ferro")
+}else if(XP >= 2001  && XP <= 2000){
+console.log("Seu nivel e  :Bronze")
+}else if(XP >= 2001  && XP <= 5000){
+    console.log("Seu nivel e :Prata")
+}else if(XP >= 5001  && XP <= 7000){
+    console.log("Seu nivel e :Ouro")
+}else if(XP >= 7001  && XP <= 8000){
+    console.log("Seu nivel e :Pratina")
+}else if(XP >= 8001  && XP <= 9000){
+    console.log("Seu nivel e :Ascendente")
+}else if(XP >= 9001  && XP <= 10000){
+    console.log("Seu nivel e :Imortal")
+}else if(XP >= 10001  ){
+    console.log("Radiante")
+}
+
+
+
